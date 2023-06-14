69e0254554a2483b8b03b823fe82016144bf5c05
darkstoorm <b.sieprawski@op.pl>
2023-06-14 19:44:17 +0200

    Extract global config

---
+++ src/Utils/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+  casingPatterns: { lower: new RegExp(/[ab]/), upper: new RegExp(/[AB]/) },
+  consonants: "bcdfghjklmnpqrstvwxz",
+  defaultNamesCount: 10,
+  defaultTemplate: "Ababbab",
+  maximumCounterInputSize: 2,
+  maximumNameLength: 20,
+  maximumNamesCount: 30,
+  maximumTemplateInputSize: 20,
+  vowels: "aeiouy",
+};
