b5ccd2c4d56b943210a76b98b554e8776cc3f6bd
darkstoorm <b.sieprawski@op.pl>
2023-06-14 15:04:19 +0200

    Add linter/formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,34 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@typescript-eslint', 'import', 'unused-imports'],
   rules: {
     'react-refresh/only-export-components': 'warn',
-  },
+    'unused-imports/no-unused-imports-ts': 2,
+    'no-eval': 2,
+    'no-implicit-coercion': 2,
+    'radix': 2,
+    'yoda': 2,
+    'no-useless-catch': 2,
+    'import/order': [
+      'error',
+      {
+        'groups': ['builtin', 'external', 'internal', 'index', 'sibling', 'parent'],
+        'alphabetize': {
+          'order': 'asc'
+        }
+      }
+    ],
+    'array-element-newline': [
+      'error',
+      {
+        'ArrayExpression': 'consistent',
+        'ArrayPattern': { 'minItems': 3 }
+      }
+    ]
+  }
 }
