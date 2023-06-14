5ab9bd88934b0421d37ce67a1ac5bba42f86e53b
darkstoorm <b.sieprawski@op.pl>
2023-06-14 15:05:04 +0200

    Add helper types

---
+++ src/Utils/Types/TCallableGetter.ts
@@ -0,0 +1,6 @@
+/**
+ * Callable method for assignments
+ *
+ * @param   {boolean}  asCapital  Flag switch for letter casing
+ */
+export type TCallableGetter = (asCapital?: boolean) => string;
---
+++ src/Utils/Types/TLettersTable.ts
@@ -0,0 +1,11 @@
+import { TCallableGetter } from "./TCallableGetter";
+
+/**
+ * Hash table of callable methods
+ */
+export type TLettersTable = {
+  a: TCallableGetter;
+  A: TCallableGetter;
+  b: TCallableGetter;
+  B: TCallableGetter;
+};
