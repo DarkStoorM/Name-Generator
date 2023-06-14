1070688b3ee30063fc7c3610860731d4c362db22
darkstoorm <b.sieprawski@op.pl>
2023-06-14 23:51:25 +0200

    Update base config

--- src/Utils/config.ts
+++ src/Utils/config.ts
@@ -1,11 +1,11 @@
 export const config = {
   casingPatterns: { lower: new RegExp(/[ab]/), upper: new RegExp(/[AB]/) },
   consonants: "bcdfghjklmnpqrstvwxz",
-  defaultNamesCount: 10,
-  defaultTemplate: "Ababbab",
-  maximumCounterInputSize: 2,
+  defaultNamesCount: 30,
+  defaultTemplate: "Abbaba Bababb",
+  maximumCounterInputSize: 3,
   maximumNameLength: 20,
-  maximumNamesCount: 30,
+  maximumNamesCount: 50,
   maximumTemplateInputSize: 20,
   vowels: "aeiouy",
 };
