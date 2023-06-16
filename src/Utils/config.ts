ffb6a7dd9e9c597e0e3f28ab64c30e8a47f9ea0b
Bartosz Sieprawski <b.sieprawski@op.pl>
2023-06-16 19:51:17 +0200

    Change default template

--- src/Utils/config.ts
+++ src/Utils/config.ts
@@ -2,7 +2,7 @@ export const config = {
   casingPatterns: { lower: new RegExp(/[ab]/), upper: new RegExp(/[AB]/) },
   consonants: "bcdfghjklmnpqrstvwxz",
   defaultNamesCount: 30,
-  defaultTemplate: "Abbaba Bababb",
+  defaultTemplate: "Abbaba Babaab",
   maximumCounterInputSize: 3,
   maximumNameLength: 20,
   maximumNamesCount: 50,
