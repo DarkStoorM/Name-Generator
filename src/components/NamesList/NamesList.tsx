f7a3d6c066ff89eccd45a8a67dbd0749e7ee015d
Bartosz Sieprawski <b.sieprawski@op.pl>
2023-06-16 19:51:26 +0200

    Add unique keys to list nodes

--- src/components/NamesList/NamesList.tsx
+++ src/components/NamesList/NamesList.tsx
@@ -5,7 +5,9 @@ export function NamesList({ names }: INameListingProps) {
   return (
     <div className="names-list">
       {names.map((name) => (
-        <div className="names-list__cell">{name}</div>
+        <div key={crypto.randomUUID()} className="names-list__cell">
+          {name}
+        </div>
       ))}
     </div>
   );
