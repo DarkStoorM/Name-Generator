705d6b0bb8836b3fb24d89af8dc10b805cb70703
Bartosz Sieprawski <b.sieprawski@op.pl>
2023-06-17 21:58:16 +0200

    Refactor config
    - uses extracted map
    - implements internal getter

--- src/Utils/config.ts
+++ src/Utils/config.ts
@@ -1,21 +1,30 @@
+import { getNumberGenerator } from "./Functions/SelectGenerator";
 import { C64 } from "./Generators/C64";
 import { Mulberry32 } from "./Generators/Mulberry32";
 import { IConfig } from "./Interfaces/IConfig";
 import { TIndexedNumberGenerators } from "./Types/TIndexedNumberGenerators";
 import { TNumberGenerator } from "./Types/TNumberGenerator";
 
-const defaultGenerator: TIndexedNumberGenerators = "mulberry32";
+/**
+ * Exposes a map of implemented random number generators
+ */
+export const availableNumberGenerators: Map<TIndexedNumberGenerators, TNumberGenerator> = new Map();
 
-const availableNumberGenerators: Record<TIndexedNumberGenerators, TNumberGenerator> = {
-  c64: C64,
-  mulberry32: Mulberry32,
-};
+/**
+ * All newly implemented random number generators have to be added to this map
+ * along with an update to the indexed generators type
+ *
+ * @see TIndexedNumberGenerators
+ */
+availableNumberGenerators.set("C64", C64);
+availableNumberGenerators.set("Mulberry32", Mulberry32);
 
 /**
  * Provides the application configs for defaults
  */
 export const config: IConfig = {
-  numberGenerator: availableNumberGenerators[defaultGenerator],
+  numberGenerator: Mulberry32,
+  currentGenerator: "Mulberry32",
   casingPatterns: { lower: new RegExp(/[ab]/), upper: new RegExp(/[AB]/) },
   consonants: "bcdfghjklmnpqrstvwxz",
   defaultNamesCount: 30,
@@ -25,4 +34,8 @@ export const config: IConfig = {
   maximumNamesCount: 50,
   maximumTemplateInputSize: 20,
   vowels: "aeiouy",
+  changeGenerator(generatorIndex: TIndexedNumberGenerators) {
+    this.currentGenerator = generatorIndex;
+    this.numberGenerator = getNumberGenerator(generatorIndex);
+  },
 };
