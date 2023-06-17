04104894fe7ff20f49d5b2d03e98bdd0c4fbdb96
Bartosz Sieprawski <b.sieprawski@op.pl>
2023-06-17 18:18:14 +0200

    Add Base number generator class

---
+++ src/Utils/Generators/BaseNumberGenerator.ts
@@ -0,0 +1,68 @@
+export abstract class BaseNumberGenerator {
+  /**
+   * The initial state stored for reset purposes
+   */
+  protected initialState = 0;
+  /**
+   * Current State
+   */
+  protected state = 0;
+
+  /**
+   * `example`: passing the callback function from inheriting class, which uses an integer as seed
+   *
+   * ```ts
+   * // Usually this should be the same for all implementations
+   * super(() => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER));
+   * ```
+   *
+   * @param   {number}  seedCallback  Callback function used to seed the new instances on the fly
+   */
+  constructor(seedCallback: () => number) {
+    this.makePseudoSeed(seedCallback);
+
+    // Avoid forcing the inheriting classes to call super.reset() on every reset implementation
+    const origin = this.reset;
+    this.reset = () => {
+      BaseNumberGenerator.prototype.reset.apply(this);
+      return origin.apply(this);
+    };
+  }
+
+  /**
+   * Returns the next random number counting from initial state
+   *
+   * `IMPORTANT NOTE`: Each PRNG class should appropriately implement `next()` method, which should
+   * return a random number from 0 to `lettersTable`'s length, otherwise the application's behavior
+   * will be unexpected
+   *
+   * @param   {string}  lettersTable  Letters table to pick a random letter from - consonants or vowels
+   * @override
+   */
+  public next = (lettersTable: string): number => {
+    return 0;
+  };
+
+  /**
+   * Resets the internals
+   *
+   * `IMPORTANT NOTE`: if the custom class implements an additional state, this method should take
+   * this into consideration and reset it. Example, C64 PRNG implements High / Low bytes, where
+   * only the lower byte is used and has to be reset
+   * @override
+   */
+  public reset = (): void => {
+    this.state = this.initialState;
+  };
+
+  /**
+   * Populates the initial/current state with a number that should be returned by the provided
+   * callback to this method
+   *
+   * @param   {number}  seedCallback  Callback to execute for initialization
+   */
+  private makePseudoSeed = (seedCallback: () => number): void => {
+    this.initialState = seedCallback();
+    this.state = seedCallback();
+  };
+}
