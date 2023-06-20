d88695f1b2b276f67722dedd3f5e22a16d8ffe39
darkstoorm <b.sieprawski@op.pl>
2023-06-20 20:25:04 +0200

    Implement new PRNGs

--- src/Utils/config.ts
+++ src/Utils/config.ts
@@ -1,9 +1,13 @@
 import { getNumberGenerator } from "./Functions/SelectGenerator";
 import { C64 } from "./Generators/C64";
+import { MultiplyWithCarry } from "./Generators/MultiplyWithCarry";
 import { Mulberry32 } from "./Generators/Mulberry32";
 import { IConfig } from "./Interfaces/IConfig";
 import { TIndexedNumberGenerators } from "./Types/TIndexedNumberGenerators";
 import { TNumberGenerator } from "./Types/TNumberGenerator";
+import { SimpleFastCounter } from "./Generators/SimpleFastCounter32";
+import { Xoshiro128 } from "./Generators/Xoshiro128";
+import { JenkinsSmallFast } from "./Generators/JenkinsSmallFast";
 
 /**
  * Exposes a map of implemented random number generators
@@ -17,7 +21,11 @@ export const availableNumberGenerators: Map<TIndexedNumberGenerators, TNumberGen
  * @see TIndexedNumberGenerators
  */
 availableNumberGenerators.set("C64", C64);
+availableNumberGenerators.set("Jenkin's Small Fast", JenkinsSmallFast);
 availableNumberGenerators.set("Mulberry32", Mulberry32);
+availableNumberGenerators.set("Multiply With Carry", MultiplyWithCarry);
+availableNumberGenerators.set("Simple Fast Counter", SimpleFastCounter);
+availableNumberGenerators.set("Xoshiro128", Xoshiro128);
 
 /**
  * Provides the application configs for defaults
